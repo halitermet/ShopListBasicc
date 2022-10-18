45c41ed55a31fdf31e63e507a4ebed73b669d35e
H <H@DESKTOP-TDSDVVD>
2022-10-18 17:45:05 +0300

    Add project files.

---
+++ ShopListBasicc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopListBasicc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
